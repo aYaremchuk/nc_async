40c84df5e12d481fdbc9b9001c7185f34ac68aac
Andrew <jaremchuk.andrij@gmail.com>
2018-09-13 08:14:50 +0000

    Add eslint

--- main.js
+++ main.js
@@ -1,65 +1,63 @@
 const request = require('request');
-var readline = require('readline-sync');
+const readline = require('readline-sync');
 
 
-const gitHubOrgName = readline.question("Type here GitHub organization name: ");
+const gitHubOrgName = readline.question('Type here GitHub organization name: ');
 
-var optionsObj = {
-    uri: '',
-    headers: {'User-Agent': 'Request-Promise' },
-    json: true
+const optionsObj = {
+  uri: '',
+  headers: { 'User-Agent': 'Request-Promise' },
+  json: true,
 };
 
 Object.defineProperty(optionsObj, 'url', {
-    set: function(value) {
-        this.url = value;
-    }
+  set(value) {
+    this.url = value;
+  },
 });
 
 optionsObj.uri = `https://api.github.com/orgs/${gitHubOrgName}/repos`;
 
 function getRepos(options) {
-    return new Promise(function(resolve, reject) {
-        request(options, function(error, response, body) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(body.slice(0, 3).map(repo => repo.name));
-            }
-        });
-    })
+  return new Promise(((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body.slice(0, 3).map(repo => repo.name));
+      }
+    });
+  }));
 }
 
 function getContributorsForRepo(options, repoName) {
-    options.uri = `https://api.github.com/repos/${gitHubOrgName}/${repoName}/contributors`;
-    return new Promise(function(resolve, reject) {
-
-        request(options, function(error, response, body) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(body.map(function(contributor) { return contributor.login; }));
-            }
-        });
-    })
+  options.uri = `https://api.github.com/repos/${gitHubOrgName}/${repoName}/contributors`;
+  return new Promise(((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body.map(contributor => contributor.login));
+      }
+    });
+  }));
 }
 
 function getContributors() {
-    var resultObj = {};
+  const resultObj = {};
 
-    getRepos(optionsObj)
-        .then(function(result) {
-            result.forEach(function (item, i, arr) {
-               getContributorsForRepo(optionsObj, item)
-                   .then(function(contributorsArray) {
-                       resultObj[item] = contributorsArray;
-                       console.log(resultObj);
-                   })
-            });
-        }
-    )
-    .catch(function (err) {
-        console.log('API call failed');
+  getRepos(optionsObj)
+    .then((result) => {
+      result.forEach((item, i, arr) => {
+        getContributorsForRepo(optionsObj, item)
+          .then((contributorsArray) => {
+            resultObj[item] = contributorsArray;
+            console.log(resultObj);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log('API call failed');
     });
 }
 
